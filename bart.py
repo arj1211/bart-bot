d455eaad299749506bb88e3eb11abba24b2c25ea
Arjun Bawa <a3bawa@uwaterloo.ca>
2021-06-24 12:58:17 -0400

    minor refactor

--- bart.py
+++ bart.py
@@ -4,15 +4,15 @@ from dotenv import load_dotenv as ldv
 
 ldv()
 
-client = discord.Client()
+bot = discord.Client()
 
-@client.event
+@bot.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+  print('We have logged in as {0.user}'.format(bot))
 
-@client.event
+@bot.event
 async def on_message(message):
-  if message.author == client.user:
+  if message.author == bot.user:
     return
 
   if message.content.startswith('$hello'):
@@ -38,9 +38,9 @@ async def on_message(message):
   if any(em in message.content for em in ['🧢','cap']):
     await message.add_reaction('🧢')
 
-@client.event
+@bot.event
 async def on_voice_state_update(member, before, after):
   # count num of users in all voice channels --> if raises to 5, send a msg, if lowers to 0, send a msg
   pass
 
-client.run(os.environ['BART_TOKEN'])
+bot.run(os.environ['BART_TOKEN'])
